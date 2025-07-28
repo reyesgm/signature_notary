acef7d27a0ca7ae94f6617ff4e60591e7a22bc68
reyesgm <100238182+reyesgm@users.noreply.github.com>
2025-07-27 23:24:59 -0700

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert("Thank you for your message! I'll get back to you shortly.");
-    this.reset();
-});
